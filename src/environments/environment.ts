f4584cf5432d4f0607db8a5f25931194287340f0
Daniel Garcia Uscanga <dalokdaga@gmail.com>
2021-05-05 23:30:53 -0500

    avance hasta el video 05 todo esta configurado y funcionando

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBa3uSYWEIsPPOp2yKu_oYPbcfTWq8qcM0",
+    authDomain: "firechat-695e3.firebaseapp.com",
+    projectId: "firechat-695e3",
+    storageBucket: "firechat-695e3.appspot.com",
+    messagingSenderId: "1089406562210",
+    appId: "1:1089406562210:web:d2cd6fb394e6b174574cfb",
+    measurementId: "G-T7HMSW6LEL"
+  }
 };
 
 /*
